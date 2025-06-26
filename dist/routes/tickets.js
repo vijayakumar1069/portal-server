c35ec8b67191779fe15902e6c8d2826c1de4f950
vijayakumar1069 <vijay.r20799@gmail.com>
2025-06-26 12:05:37 +0530

    dist added in gitignore

--- dist/routes/tickets.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { authenticateToken } from '../middleware/auth.js';
-import { getTicketContactController, getTicketDetailsController, getTicketsController, } from '../controllers/ticket.controller.js';
-const ticketRoutes = express.Router();
-ticketRoutes.get('/', authenticateToken, getTicketsController);
-ticketRoutes.get('/:ticketId', authenticateToken, getTicketDetailsController);
-ticketRoutes.get('/:ticketId/contact', authenticateToken, getTicketContactController);
-export default ticketRoutes;
